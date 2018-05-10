20bbc861d5e0946e61573499b1a19060aeb35f39
Martin Holden <martin.holden@skillsoft.com>
2018-05-10 17:49:35 +0100

    Update of local copies and loaders
    Adding updated local copies of jquery and crypto-js. Updating loaders.

--- libs/crypto-js-loader.js
+++ libs/crypto-js-loader.js
@@ -1,11 +1,11 @@
 (function() {
   // Default to the local version.
-  var path = '../libs/jquery/jquery.js';
+  var path = '../libs/crypto-js/crypto-js.js';
   // Get any jquery=___ param from the query string.
-  var jqversion = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
+  var cryptojsversion = location.search.match(/[?&]cryptojs=(.*?)(?=&|$)/);
   // If a version was specified, use that version from code.jquery.com.
-  if (jqversion) {
-    path = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/' + jqversion[1] + '/jquery.js';
+  if (cryptojsversion) {
+    path = 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/' + jqversion[1] + '/crypto-js.js';
   }
   // This is the only time I'll ever use document.write, I promise!
   document.write('<script src="' + path + '"></script>');
